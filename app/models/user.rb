d12f56d446bb8a4cb6da72d9f4afab29255168bf
Rupak Ganguly <rupakg@gmail.com>
2012-08-31 00:40:48 -0400

    Override devise sign_in redirect to go to new athlete page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,6 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
   
-  # has_one :athlete, dependent: :destroy
+  has_one :athlete, dependent: :destroy
 
-  
 end
