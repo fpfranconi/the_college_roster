5b8afc67183b26786e60a3c658bc64b97ea23e02
Rupak Ganguly <rupakg@gmail.com>
2012-09-11 01:28:09 -0400

    Fixes issue #1.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     if current_user.athlete && Athlete.exists?(current_user.athlete.id)
-      edit_athlete_path(current_user.athlete.id)
+      athlete_path(current_user.athlete.id)
     else
       new_athlete_path
     end
