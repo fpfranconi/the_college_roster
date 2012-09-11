2ed49dcd3dae733a4a284cd2e5f00d101c5e5bfe
Rupak Ganguly <rupakg@gmail.com>
2012-09-11 01:20:01 -0400

    Fix issue when new user signs in and athlete record is not present.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def after_sign_in_path_for(resource)
-    if Athlete.exists?(current_user.athlete.id)
+    if current_user.athlete && Athlete.exists?(current_user.athlete.id)
       edit_athlete_path(current_user.athlete.id)
     else
       new_athlete_path
